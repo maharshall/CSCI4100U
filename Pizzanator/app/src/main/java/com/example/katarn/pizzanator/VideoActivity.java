7afdfe94bc0a7d5d69a5d7986fb722d8bdf720b7
Hozukimaru <dylancrawford113@gmail.com>
2015-11-28 15:07:01 -0500

    video activity

--- Pizzanator/app/src/main/java/com/example/katarn/pizzanator/VideoActivity.java
+++ Pizzanator/app/src/main/java/com/example/katarn/pizzanator/VideoActivity.java
@@ -1,25 +1,35 @@
 package com.example.katarn.pizzanator;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-public class MainActivity extends Activity {
+public class VideoActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_video);
+
+        final VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        /*
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.potato);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        videoView.start();*/
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_video, menu);
         return true;
     }
 
@@ -36,14 +46,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void findPizza(View V) {
-
-    }
-
-    public void tutorial(View V) {
-        Intent intent = new Intent(this,VideoActivity.class);
-        startActivity(intent);
     }
 }
